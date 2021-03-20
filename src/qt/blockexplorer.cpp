7634b2c91d6d7554f4cc394822768b87b2363cb0
crypto-node <crypto-node@gmx.net>
2021-03-21 00:02:29 +0100

    [QT]color set update

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -548,7 +548,7 @@ void BlockExplorer::setBlock(CBlockIndex* pBlock)
 
 void BlockExplorer::setContent(const std::string& Content)
 {
-    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#005437;}\n a, span { font-family: monospace; }\n span.addr {color:#005437; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #005437;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#005437;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#005437;}\n";
+    QString CSS = "body {font-size:12px; color:#f7f7f8; bgcolor:#9b0000;}\n a, span { font-family: monospace; }\n span.addr {color:#843534; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #843534;}\n td.d0 {font-weight: bold; color:#f7f7f9;}\n h2, h3 { white-space:nowrap; color:#843534;}\n a { color:#f7f7f9; text-decoration:none; }\n a.nav {color:#333333;}\n";
     QString FullContent = "<html><head><style type=\"text/css\">" + CSS + "</style></head>" + "<body>" + Content.c_str() + "</body></html>";
     // printf(FullContent.toUtf8());
 
